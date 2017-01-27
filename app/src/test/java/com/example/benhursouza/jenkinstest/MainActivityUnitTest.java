4b82b7416ef291e42c00cf963e95668bf5bfc182
Benhur Souza <benhur.souza@m4ucorp.dmc>
2017-01-27 18:14:06 -0200

    test error

--- app/src/test/java/com/example/benhursouza/jenkinstest/MainActivityUnitTest.java
+++ app/src/test/java/com/example/benhursouza/jenkinstest/MainActivityUnitTest.java
@@ -24,10 +24,10 @@ public class MainActivityUnitTest {
 //    private TextView txt;
 //    private Button button;
 
-//    @Test
-//    public void addition_isCorrect() throws Exception {
-//        assertEquals(4, 2 + 2);
-//    }
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
 
 //    @Before
 //    public void setUp() {
